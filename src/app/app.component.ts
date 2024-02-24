4bd34ce4816a00ec80688ff4d637441a91c9746e
Claude Mokbel <claudemokbel@gmail.com>
2024-02-24 10:26:31 -0800

    more boiler plate added, some files deleted

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NavigationBarComponent } from './shared/navigation-bar/navigation-bar.component';
+import { NavigationFooterComponent } from './shared/navigation-footer/navigation-footer.component';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NavigationBarComponent, NavigationFooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
 export class AppComponent {
+  loading: Observable<boolean> | undefined
   title = 'angular-blog';
 }
