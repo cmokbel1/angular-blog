509f893f39b73e1411cc9f8cab60cbf5e8fdacbe
cmokbel1 <claudemokbel@gmail.com>
2026-02-28 16:14:41 -0800

    fix an issue causing the footer to hide content from view on the page

--- src/app/shared/navigation-bar/navigation-bar.component.ts
+++ src/app/shared/navigation-bar/navigation-bar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-navigation-bar',
   standalone: true,
   templateUrl: './navigation-bar.component.html',
   styleUrl: './navigation-bar.component.scss',
-  imports: [RouterLink],
+  imports: [RouterLink, ButtonModule],
 })
 export class NavigationBarComponent {}
